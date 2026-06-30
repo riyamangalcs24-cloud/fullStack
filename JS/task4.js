36f009883813dd765210633d09c48cfb02cd728b
riya <mangalriya30@gmail.com>
2026-06-30 19:03:27 +0530

    Task 4 about if-else

---
+++ JS/task4.js
@@ -0,0 +1,38 @@
+// const user = 82;
+// if(user >= 90){
+//     console.log("A+");
+// }
+// else if(user >= 80){
+//     console.log("B");
+// }
+// else if(user >= 70){
+//     console.log("C");
+// }
+// else if(user >= 60){
+//     console.log("D");
+// }
+// else{
+//     console.log("F");
+// }
+
+// const dayNumber = 5;
+// switch(dayNumber){
+//     case 1:console.log("Monday");
+//     break;
+//     case 2:console.log("Tuesday");
+//     break;
+//     case 3:console.log("Wednesday");
+//     break;
+//     case 4:console.log("Thrusday");
+//     break;
+//     case 5:console.log("Friday");
+//     break;
+//     case 6:console.log("Saturday");
+//     break;
+//     default:console.log(Sunday);
+// }
+
+
+const dayNumber = 6;
+const isWeekend = (dayNumber === 6 || dayNumber === 7) ? 'yes' : 'No' ;
+console.log(isWeekend);
