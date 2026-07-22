b784cded0898e3ff988991329d8155a7e1fde038
riya <mangalriya30@gmail.com>
2026-07-22 12:57:07 +0530

    YE

--- Backend/script.js
+++ Backend/script.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 app.use(bodyParser.json()); // middleware
 
 app.get("/",(req,res) => {
@@ -13,12 +14,19 @@ app.get("/data",(req,res) => {
 })
 
 app.post("/user",(req,res) => {
-    const {name,section} = req.body;
+    const { name,section } = req.body;
     console.log(name);
     console.log(section);
     res.send("Data Recived");
 })
 
+
+mongoose.connect("mongodb://localhost:27017/mydata").then(() => {
+    console.log("DB Connected sucessfully");
+}).catch((error) => {
+    console.log(error);
+    console,log("DB NOT CONNEcted");
+})
 app.listen(3000,() => {
     try{
         console.log(`http://localhost:${PORT}`);
